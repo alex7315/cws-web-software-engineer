eae76c355e0f3a4e06fd54366f69b2648f32a6f5
alexey <klychko@smatos.de>
2024-10-24 12:06:56 +0200

    CWSETT-5 adds logger configuration

---
+++ web-software-engineer-task/web-software-engineer-task-sync/src/main/java/org/cws/web/software/engineer/task/sync/controller/JobSchedulerController.java
@@ -0,0 +1,37 @@
+package org.cws.web.software.engineer.task.sync.controller;
+
+import org.cws.web.software.engineer.task.sync.scheduler.GithubUsersSyncJobScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job")
+public class JobSchedulerController {
+
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
+	@PutMapping("/scheduler/activate")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void activate() {
+		GithubUsersSyncJobScheduler scheduler = applicationContext.getBean(GithubUsersSyncJobScheduler.class);
+		scheduler.enable();
+	}
+
+	@PutMapping("/scheduler/deactivate")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deactivate() {
+		GithubUsersSyncJobScheduler scheduler = applicationContext.getBean(GithubUsersSyncJobScheduler.class);
+		scheduler.disable();
+	}
+
+}
