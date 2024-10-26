3201b9bd1ac11ef30e1cf4b6511762fe67144fe6
Alexey Klychko <klychko@smatos.de>
2024-10-26 15:00:34 +0200

    CWSETT-7 implements security classes and configures backend security

--- web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/config/SecurityConfig.java
+++ web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/config/SecurityConfig.java
@@ -1,15 +1,69 @@
 package org.cws.web.software.engineer.task.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableMethodSecurity /*(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)*/
+@EnableMethodSecurity
+//(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 //by default
 public class SecurityConfig {
-	/*
-	 * @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	 * http.authorizeHttpRequests((authz) ->
-	 * authz.anyRequest().authenticated()).oauth2Login(); return http.build(); }
-	 */
+
+    @Autowired
+    @Qualifier("userDetailsServiceImpl")
+    UserDetailsService        userDetailsService;
+
+    @Autowired
+    private JwtAuthEntryPoint unauthorizedHandler;
+
+    @Bean
+    AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //@formatter:off
+        http.csrf(csrf -> csrf.disable())
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> 
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/test/**").permitAll()
+                        .anyRequest().authenticated()
+                    );
+    
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    
+        return http.build();
+        //@formatter:on
+    }
 }
---
+++ web-software-engineer-task/web-software-engineer-task-security/src/main/java/org/cws/web/software/engineer/task/security/jwt/JwtAuthEntryPoint.java
@@ -0,0 +1,31 @@
+package org.cws.web.software.engineer.task.security.jwt;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * This class implements {@link AuthenticationEntryPoint} interface and used to handle authentication exception.
+ * Class overrides {@code commence()} method that is triggered any time unauthenticated User requests a secured HTTP resource
+ * and an {@link AuthenticationException} is thrown.
+ */
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JwtAuthEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        LOG.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+
+}
