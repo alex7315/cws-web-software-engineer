38b9c29b65aa3923ffa586b7bf0f47f45f4f83fc
Alexey Klychko <klychko@smatos.de>
2024-10-30 15:46:53 +0100

    CWSETT-7 adds default constructor to allow serialization of JwtResponse

--- web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/dto/response/JwtResponse.java
+++ web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/dto/response/JwtResponse.java
@@ -3,10 +3,14 @@ package org.cws.web.software.engineer.task.backend.dto.response;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class JwtResponse {
 
