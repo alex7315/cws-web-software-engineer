3ae98ce0c570de19c723f71db1130b8239d0fa01
Alexey Klychko <klychko@smatos.de>
2024-10-30 14:55:39 +0100

    CWSETT-7 adds spring boot application to test package

---
+++ web-software-engineer-task/web-software-engineer-task-security/src/test/java/org/cws/web/software/engineer/task/security/TestApplication.java
@@ -0,0 +1,8 @@
+package org.cws.web.software.engineer.task.security;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+}
