45a5505fa9757e8483e257615c075623088df050
Alexey Klychko <klychko@smatos.de>
2024-11-04 11:21:53 +0100

    CWSETT-8 adds CORS configuration

--- web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/WebSoftwareEngineerTaskBackendApplication.java
+++ web-software-engineer-task/web-software-engineer-task-backend/src/main/java/org/cws/web/software/engineer/task/backend/WebSoftwareEngineerTaskBackendApplication.java
@@ -3,7 +3,10 @@ package org.cws.web.software.engineer.task.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = { "org.cws.web.software.engineer.task.persistence.repository" })
@@ -13,5 +16,20 @@ public class WebSoftwareEngineerTaskBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebSoftwareEngineerTaskBackendApplication.class, args);
 	}
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //@formatter:off
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("*");
+                //@formatter:on
+            }
+        };
+    }
 
 }
