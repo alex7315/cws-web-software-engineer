e3c77558774a07622661129c8e1db6390df4d1aa
alexey <klychko@smatos.de>
2025-08-24 18:19:32 +0200

    CWSETT-24 allows exposition of some actuator endpoints and adds aspect configuration for actuator metrics

---
+++ web-software-engineer-task-backend/web-software-engineer-task-sync/src/main/java/org/cws/web/software/engineer/task/sync/configuration/MicrometerAspectConfiguration.java
@@ -0,0 +1,19 @@
+package org.cws.web.software.engineer.task.sync.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+
+/**
+ * This configuration defines aspects like {@link TimedAspect} allows processing
+ * annotations like @Timed, that adds Micrometer custom metric(e.g. Timer)
+ */
+@Configuration
+public class MicrometerAspectConfiguration {
+	@Bean
+	TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
+}
