74c95ea4e044beaee2813eca34c083a737fb7222
alexey <klychko@smatos.de>
2025-08-25 12:10:21 +0200

    CWSETT-24 adds micrometer for aspects timer and counter

--- web-software-engineer-task-backend/web-software-engineer-task-sync/src/main/java/org/cws/web/software/engineer/task/sync/configuration/MicrometerAspectConfiguration.java
+++ web-software-engineer-task-backend/web-software-engineer-task-sync/src/main/java/org/cws/web/software/engineer/task/sync/configuration/MicrometerAspectConfiguration.java
@@ -3,6 +3,7 @@ package org.cws.web.software.engineer.task.sync.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.micrometer.core.aop.CountedAspect;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 
@@ -15,5 +16,10 @@ public class MicrometerAspectConfiguration {
 	@Bean
 	TimedAspect timedAspect(MeterRegistry registry) {
 		return new TimedAspect(registry);
+	}
+
+	@Bean
+	CountedAspect countedAspect(MeterRegistry registry) {
+		return new CountedAspect(registry);
 	}
 }
